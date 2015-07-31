6dfdc648482bbbab3ce615c6cb4b2aec138c30c7
Masaaki Morishita <ikasama3.0@gmail.com>
2015-08-01 07:03:22 +0900

    implement

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-module.exports = function () {
+var isCombinator = require('is-css-combinator')
+
+module.exports = function (selector) {
+    selector.split('').forEach(function (char) {
+        if (isCombinator(char)) return true
+    })
+    return false
 }
